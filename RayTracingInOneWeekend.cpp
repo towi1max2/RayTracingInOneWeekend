eafd50be3aa10eb961fdcf3d9410837e06634f46
enrique guardado <coco1max2@gmail.com>
2025-10-08 23:18:46 -0700

    added some notes

--- RayTracingInOneWeekend.cpp
+++ RayTracingInOneWeekend.cpp
@@ -1,7 +1,8 @@
-
+//idk why we need to define these for the stb image write header to not bug out?
 #define _CRT_SECURE_NO_WARNINGS
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-//$(SolutionDir)Source;%(AdditionalIncludeDirectories) An additional root for the compiler to in for the correct files
+
+//$(SolutionDir)Source;%(AdditionalIncludeDirectories) An additional root for the compiler to look in to find the correct files
 #include "ThirdParty/stb_image_write.h"
 
 #include <iostream>
@@ -14,15 +15,22 @@
 int main() 
 {
 
-	// Image woot woot
-
+	// Image
 	int image_width = 1920;
 	int image_height = 1080;
-	int comp = 3;
-	int stride = image_width * comp;
+	int ColorChannels = 4;
+	int stride = image_width * ColorChannels;
 
 	//Buffer
-	std::vector<unsigned char> pixels(image_width * image_height * comp);
+	/*
+	std::vector has nothing to do with the 3 component "vector" from math. std:vector should be seen as a more advance version of a regular array, by being resizable and smart.
+	
+	ClassTemplate -> std::vector<> = is a dynamic vector array that can change size easily, knows its size, manages its own memory etc easy to use.
+	SpecificClassType -> typename -> unsigned char = is a data type that stores numbers between 0 - 255
+	Object/Instance of the Class -> Pixels
+	pixel(...) <- The integer going in is simply setting the intial size in memory bytes for the vector object to hold.
+	*/
+	std::vector<unsigned char> pixels(image_width * image_height * ColorChannels);
 
 	// Render
 
@@ -36,31 +44,33 @@ int main()
 		{
 			auto coordX = double(pixelX) / (image_width - 1);//gives normalized - to 1 repeating by heights times
 			auto coordY = double(pixelY) / (image_height - 1);
-			auto coordZ = 0.0; //a decimal makes it a double automatially
 
-			int red = int(255.999 * coordX);
-			int green = int(255.999 * coordY);
-			int blue = int(255.999 * coordZ);
+			unsigned char red = int(255 * coordX);
+			unsigned char green = int(255 * coordY);
+			unsigned char blue = 0;
+			unsigned char alpha = 255;
 
-			//std::cout << red << ' ' << green << ' ' << blue << "\n";
-
-			int idx = (pixelY * image_width + pixelX) * comp;
-			pixels[idx + 0] = (unsigned char)red;
-			pixels[idx + 1] = (unsigned char)green;
-			pixels[idx + 2] = (unsigned char)blue;
+			/* RootPixelIndex -> its the first array index of the current pixel
+			* pixelY * image_width -> this part is the next row line
+			* + pixelX -> this moves the selection to the right
+			* (...) * comp -> this leaves gaps for the distinct color channels to fill inbetween
+			*/
+			int RootPixelIndex = (pixelY * image_width + pixelX) * ColorChannels;
+			pixels[RootPixelIndex + 0] = red;
+			pixels[RootPixelIndex + 1] = green;
+			pixels[RootPixelIndex + 2] = blue;
+			pixels[RootPixelIndex + 3] = alpha;
 
 		}
 	}
-	stbi_write_png("Render/Image.png", image_width, image_height, comp, pixels.data(), stride);
+	/* stbi_write_png function usage
+	filename -> Output path for the PNG File
+	x -> width of the image in pixels
+	y -> height of the image in pixels
+	comp -> number of color channels
+	*data -> pointer to a 1D array(organized correctly) that holds pixel data
+	stride_bytes -> the number of bytes per row in the image... + optionally some more for padding to do more advanced memory layouts.
+	*/
+	stbi_write_png("Render/Image.png", image_width, image_height, ColorChannels, pixels.data(), stride);
 
 }
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
